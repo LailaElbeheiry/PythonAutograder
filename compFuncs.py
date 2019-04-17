89eacd688548b7523fa6db0751684f4c480c0471
LailaElbeheiry <osslaila@gmail.com>
2019-04-17 08:31:25 +0300

    Update compFuncs.py

--- compFuncs.py
+++ compFuncs.py
@@ -5,14 +5,9 @@ def CompFloats(a,b):
     except:
         return False
     return abs(a-b)<0.0005
-#def compEqual(x,y):
-#    print "x: ",x
-#    print "y: ",y
-#    return x==y
+
 
 COMPFUNCS = []
 COMPFUNCS.append(lambda x,y: x==y)
 COMPFUNCS.append(CompFloats)
 
-###COMPFUNCS = [equality, floats]
-###INDICES =        0       1
