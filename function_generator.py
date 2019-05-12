1bdffbdacad73095a3b865a8049f792cc21a4e3d
LailaElbeheiry <osslaila@gmail.com>
2019-05-12 19:23:00 +0300

    minor modifications

--- function_generator.py
+++ function_generator.py
@@ -1,62 +1,127 @@
-# -*- coding: utf-8 -*-
-"""
-@author: LailaElbeheiry
-"""
-
-
-import math
-import lab1
-
-"""
-This file should be editted to generate the autograding files
-In line 31, insert the function names in the list "names"
-
-In line 33, insert the name of the first function
-
-In lines 38 and 44, edit the function refFunWrapper, so that it correctly  
-casts the input to the actual input types
-
-In line 41, edit the refFun so that it has the body for the reference function 
-for that particular class
-
-Run this file to generate the autograders for each function
-
-Check the file template_generator.py for reference
-"""
-
-
-
-#####################################################################################
-####THIS PART SHOULD BE EDITTED AS DESCRIBED ABOVE
-names = ["insert function 1 name", "insert function 2 name"]
-
-class insertClass1Name:
+class whichTriangle:
+    '''add your imports here
+    if there are no imports, uncomment pass'''
     def __init__(self):
-        pass
-        
+        import math
+        ##add more imports
+        #pass
+    
+    '''this function is a wrapper around the reference function
+    Add input parameters and cast them to their types.
+    DONOT CHANGE THE NAME OF THE FUNCTION'''
     def refFuncWrapper(self):
-        return self.refFunc( int(raw_input()), int(raw_input()), int(raw_input()) )
-    
-    def refFunc(self,inp1,inp2,inp3):
-        return "insert reference function body"
-    
-    def inputFunc(self):
-        x =  lab1.whichTriangle( int(raw_input()), int(raw_input()), int(raw_input()) )
-        return x
-    
-#####################################################################################
-
-
-####THIS PART SHOULD NOT EDITTED AS DESCRIBED ABOVE
-def generate(names):
-    src = open("source.py")
-    srcLines = list(map(lambda x: x.rstrip('\0'), src.readlines()))
-    for name in names:
-        f = open(name + ".py", 'w')
-        srcLines[1] = "from functionGenerator import " + name + " as s"
-        f.writelines(srcLines)
-        f.close()   
+        return self.refFunc( int(raw_input()), int(raw_input()), int(raw_input()) ) #add more parameters here
+ 
+    '''this is the reference function. Add input parameters and change function
+    body. DONOT CHANGE THE NAME OF THE FUNCTION. You can change the names of 
+    the input parameters'''
+    def refFunc(self,a,b,c):
+        return 77
+        if a <= 0 or b <= 0 or c <= 0: # sides are negative
+            return 0
+        if ((a+b)<=c or (a+c)<=b or (b+c)<=a): # sides don't add up
+            return 0
+        # Right triangle
+        if (a**2+b**2)==c**2 or (a**2+c**2)==b**2 or (b**2+c**2)==a**2:
+            return 1
+        if a==b and b==c: # Equilateral triangles
+            return 3
+        if self.isObtuse(a,b,c) and not self.isIsosceles(a,b,c): # Obtuse scalene triangle
+            return 4
+        if self.isObtuse(a,b,c) and self.isIsosceles(a,b,c): # Obtuse isosceles triangle
+            return 5
+        if self.isAcute(a,b,c) and not self.isIsosceles(a,b,c): # Acute scalene triangle
+            return 6
+        if self.isAcute(a,b,c) and self.isIsosceles(a,b,c): # Acute isosceles triangle
+            return 7    
+            
+    def isAcute(self,a,b,c):
+        if (a**2+b**2)>c**2 or (a**2+c**2)>b**2 or (b**2+c**2)>a**2:
+            return True
+        return False 
         
-if __name__ == "__main__":
-    generate(names)    
+    def isObtuse(self,a,b,c):
+        if (a**2+b**2)<c**2 or (a**2+c**2)<b**2 or (b**2+c**2)<a**2:
+            return True
+        return False
+        
+    def isIsosceles(self,a,b,c):
+        if a==b or b==c or a==c:
+            return True
+        return False
+
+        
+                 
+    '''THIS SHOULD BE SIMILAR TO refFuncWrapper. This will be used to call
+    the function that is being graded. DONOT CHANGE ANYTHING OTHER THAN
+    THE INPUT PARAMETERS'''
+    def inputFunc(self):
+        import lab1
+        return lab1.whichTriangle( int(raw_input()), int(raw_input()), int(raw_input()) )
+
+
+class isFloat:
+    '''add your imports here
+    if there are no imports, uncomment pass'''
+    def __init__(self):
+        ##add more imports
+        pass
     
+    '''this function is a wrapper around the reference function
+    Add input parameters and cast them to their types.
+    DONOT CHANGE THE NAME OF THE FUNCTION'''
+    def refFuncWrapper(self):
+        return self.refFunc( raw_input() ) #add more parameters here
+ 
+    '''this is the reference function. Add input parameters and change function
+    body. DONOT CHANGE THE NAME OF THE FUNCTION. You can change the names of 
+    the input parameters'''
+    def refFunc(self,string):
+    
+        string = self.clearSpace(string)
+        # Clear signs on the first character
+        if string[0] == '+' or string[0] == '-':
+            string = string[1 : len(string)]
+        # Check dot
+        dotPos = [] #stores the indices of dot
+        for i in range(len(string)):
+            if string[i] == '.':
+                dotPos.append(i)
+        if len(dotPos) > 1: #there should not be more than 1 dot
+            return False
+        # Check if are numbers or dot
+        numbers = ['0','1','2','3','4','5','6','7','8','9','.']
+        for character in string:
+             if character not in numbers:
+                 return False
+        return True
+ 
+    def clearSpace(self, string): # clear leading and trailing spaces
+        # Clear spaces in front the number
+        isSpace = True
+        counter = 0
+        while(isSpace and counter<len(string)):
+            if string[counter] == ' ':
+                counter += 1
+            else:
+                isSpace = False
+        string = string[counter:len(string)]
+        # Clear spaces trailing the number
+        isSpace = True
+        counter = len(string) - 1
+        while(isSpace and counter > 0): # no need to check string[0]
+            if string[counter] == ' ':
+                counter -= 1
+            else:
+                isSpace = False
+        string = string[0:counter+1] 
+        return string
+ 
+ 
+ 
+    '''THIS SHOULD BE SIMILAR TO refFuncWrapper. This will be used to call
+    the function that is being graded. DONOT CHANGE ANYTHING OTHER THAN
+    THE INPUT PARAMETERS'''
+    def inputFunc(self):
+        import lab1
+        return lab1.isFloat( raw_input() )
